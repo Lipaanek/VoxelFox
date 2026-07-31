3da9d378c3262229846eb043eb9e16650231edc6
Lipaanek <lipovskymatyas@gmail.com>
2026-07-31 23:15:59 +0200

    Renamed shader files

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,8 @@ int main() {
 
     // Shader program and shader creation
     ShaderProgram program;
-    Shader frag("assets/shaders/test_frag.glsl", ShaderType::Fragment);
-    Shader vert("assets/shaders/test_vert.glsl", ShaderType::Vertex);
+    Shader frag("assets/shaders/3d_scene_frag.glsl", ShaderType::Fragment);
+    Shader vert("assets/shaders/3d_scene_vert.glsl", ShaderType::Vertex);
 
     // Need to compile the shaders before linking
     frag.compile();
